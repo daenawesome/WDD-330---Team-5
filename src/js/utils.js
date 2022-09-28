5d458d2efacfbd8442cae3bcd19a572448452c6c
Daen Antule <ant21006@byui.edu>
2022-09-28 04:29:25 +0300

    Activity3 & added some comments for easy read

--- src/js/utils.js
+++ src/js/utils.js
@@ -1,9 +1,13 @@
+"use strict";
+// It eliminates silent errors and instead throws them so that 
+// the code won't run with errors in the code.
+
 // wrapper for querySelector...returns matching element
+// or a more concise version:
+// export const qs = (selector, parent = document) => parent.querySelector(selector);
 export function qs(selector, parent = document) {
   return parent.querySelector(selector);
 }
-// or a more concise version if you are into that sort of thing:
-// export const qs = (selector, parent = document) => parent.querySelector(selector);
 
 export function getParam(param) {
   const queryString = window.location.search;
@@ -15,10 +19,12 @@ export function getParam(param) {
 export function getLocalStorage(key) {
   return JSON.parse(localStorage.getItem(key));
 }
+
 // save data to local storage
 export function setLocalStorage(key, data) {
   localStorage.setItem(key, JSON.stringify(data));
 }
+
 // set a listener for both touchend and click
 export function setClick(selector, callback) {
   qs(selector).addEventListener("touchend", (event) => {
@@ -26,4 +32,14 @@ export function setClick(selector, callback) {
     callback();
   });
   qs(selector).addEventListener("click", callback);
+}
+
+// Make it reusable
+// to render data to a template 
+export function renderListWithTemplate(template, parentElement, list, callback) {
+  list.forEach(item => {
+    const clone = template.content.cloneNode(true);
+    const doneTemplate = callback(clone, item);
+    parentElement.appendChild(doneTemplate);
+  })
 }
